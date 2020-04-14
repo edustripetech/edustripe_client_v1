c8c4ff8e932b0053c4f76a39ca8ad90bdddd45c8
martins obayomi <obayomimartins96@gmail.com>
2020-04-14 14:11:11 +0100

    Create step one signup page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './views/Home';
 import Login from './views/Authentication/Login';
+import Signup from './views/Authentication/Signup';
 import ParentSignup from './views/Authentication/ParentSignup';
 import StudentSignup from './views/Authentication/StudentSignup';
 
@@ -14,6 +15,7 @@ const App = () => {
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/login' exact component={Login} />
+          <Route path='/signup' exact component={Signup} />
           <Route path='/parent-signup' exact component={ParentSignup} />
           <Route path='/student-signup' exact component={StudentSignup} />
         </Switch>
