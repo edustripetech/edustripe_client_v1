cd820adcdd593b04c2a4014ae963027b0dfcbade
Adeola Adeyemo <clasiqaas@gmail.com>
2020-12-31 16:01:01 +0100

    implement partner and featured sections and add svg files

---
+++ src/assets/svg/index.js
@@ -0,0 +1,16 @@
+import BusinessDay from "./BusinessDay";
+import InnovationVillage from "./InnovationVillage";
+import Meristem from "./Meristem";
+import Monnify from "./Monnify";
+import Providus from "./Providus";
+import TechCity from "./TechCity";
+
+
+export {
+  BusinessDay,
+  InnovationVillage,
+  Meristem,
+  Monnify,
+  Providus,
+  TechCity
+};
