3fd4a01435bd860cce9b33a987c20d40dc2f7b69
Martins Obayomi <obayomimartins96@gmail.com>
2021-02-15 16:18:12 +0100

    update login & signup links

--- src/App.js
+++ src/App.js
@@ -7,22 +7,6 @@ const Home = lazy(() =>
   import(/* webpackChunkName: "Home" */ './views/Home')
 );
 
-const Login = lazy(() =>
-  import(/* webpackChunkName: "Login" */ './views/Authentication/Login')
-);
-
-const Signup = lazy(() =>
-  import(/* webpackChunkName: "Signup" */ './views/Authentication/Signup')
-);
-
-const ParentSignup = lazy(() =>
-  import(/* webpackChunkName: "ParentSignup" */ './views/Authentication/ParentSignup')
-);
-
-const StudentSignup = lazy(() =>
-  import(/* webpackChunkName: "StudentSignup" */ './views/Authentication/StudentSignup')
-);
-
 const AboutUs = lazy(() =>
   import(/* webpackChunkName: "AboutUs" */ './views/AboutUs')
 );
@@ -38,10 +22,6 @@ const App = () => {
         <Suspense fallback={<h3 style={{ width: '100%', margin: '20% auto', textAlign: 'center', display: 'flex', justifyContent: 'center' }}>Loading...</h3>}>
           <Switch>
             <Route path='/' exact component={Home} />
-            <Route path='/login' exact component={Login} />
-            <Route path='/signup' exact component={Signup} />
-            <Route path='/parent-signup' exact component={ParentSignup} />
-            <Route path='/student-signup' exact component={StudentSignup} />
             <Route path='/about' exact component={AboutUs} />
             <Route path='/privacy' exact component={Privacy} />
           </Switch>
